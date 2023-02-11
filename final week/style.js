802348ce420a441c3752ae2185fa41018f90fbd4
Viraj subedi <118783930+virajsubedi@users.noreply.github.com>
2023-02-11 15:55:57 +0545

    javascript
    applying completely JavaScript

--- final week/style.js
+++ final week/style.js
@@ -1,18 +1,31 @@
-$('form').submit(function () {
-    // Get the Login Name value and trim it
-        var fname = $.trim($('#fname').val());
-        var lname = $.trim($('#lname').val());
-        var email =$.trim($('#email').val());
-    // Check if empty of not
-        if (fname === '') {
-            alert('First name is empty.');
-            return false;
-        }
-        else if (lname === '') {
-            alert('Last Name is empty.');
-            return false;
-        }
-        else if (email === '') {
-            alert('email is empty.');
-            return false;
-        }
+$('document').ready(function(){
+	$('input[type="text"], input[type="email"], textarea').focus(function(){
+		var background = $(this).attr('id');
+		$('#' + background + '-form').addClass('formgroup-active');
+		$('#' + background + '-form').removeClass('formgroup-error');
+	});
+	$('input[type="text"], input[type="email"], textarea').blur(function(){
+		var background = $(this).attr('id');
+		$('#' + background + '-form').removeClass('formgroup-active');
+	});
+
+function errorfield(field){
+	$(field).addClass('formgroup-error');
+	console.log(field);	
+}
+
+$("#waterform").submit(function() {
+	var stopsubmit = false;
+
+if($('#name').val() == "") {
+	errorfield('#name-form');
+	stopsubmit=true;
+}
+if($('#email').val() == "") {
+	errorfield('#email-form');
+	stopsubmit=true;
+}
+  if(stopsubmit) return false;
+});
+		
+});
